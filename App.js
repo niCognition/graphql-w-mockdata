04cae6c3eb638517076d9208231eae9ee6e8446e
niCognition <Niclas.p1990@gmail.com>
2021-05-17 14:01:37 +0200

    add cors library

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
-const port = 3000;
+const port = 6969;
 const { graphqlHTTP } = require('express-graphql');
 const schema = require("./Schemas/index.js");
 
+app.use(cors({ origin: 'http://localhost:3000', credentials: true}))
 app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: true
-}))
+}));
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`)
-})
+});
